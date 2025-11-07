fab826efa19206096feb9aa82a56abe1bdb862fc
hj <h-ja@qq.com>
2025-11-07 10:13:51 +0800

    refactor: 移除less配置并更新依赖项
    将antd样式从less改为css，并移除相关less配置
    更新mongoose和concurrently依赖版本
    修改代理端口并添加babel配置

--- config-overrides.js
+++ config-overrides.js
@@ -2,15 +2,9 @@
   此文件用于给react-app-rewired修改create-react-app脚手架的默认配置
  */
 const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
-  config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-  config = rewireLess(config, env, {
-    modifyVars: {
-      '@primary-color': '#1DA57A',
-      '@font-size-base': '14px',
-   },
-  });
+  // 使用css而不是less
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], config);
   return config;
 };
